e1bcf78bb43aec3efa19e83a18fd9558f603e57f
thdoenle <110640532+thdoenle@users.noreply.github.com>
2024-06-23 20:23:00 +0200

    Module06 - ex01 main done

--- Module06/ex01/main.cpp
+++ Module06/ex01/main.cpp
@@ -4,4 +4,20 @@
 
 int main(void)
 {
+  Data data;
+
+  data.l = 42000000000;
+  data.i = -42;
+  data.d = 0.42!
+  uintptr_t rawPtr = Serializer::serialize(&data);
+  Data *ptr = Serializer::deserialize(uintptr_t rawPtr);
+
+  std::cout << "Addresses: << std::endl;
+  std::cout << &data << std::endl;
+  std::cout << rawPtr << std::endl;
+  std::cout << ptr << std::endl;
+  std::cout << "Values: << std::endl;
+  std::cout << ptr->l << std::endl;
+  std::cout << ptr->i << std::endl;
+  std::cout << ptr->d << std::endl;
 }
