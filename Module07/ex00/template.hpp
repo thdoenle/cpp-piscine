6e988e67f9ca04a6f33243414bc93b79a3d7642b
Thibaud Doenlen--Chiésa <thibaud42@protonmail.com>
2024-07-13 20:22:18 +0200

    Module07 - ex00 done

---
+++ Module07/ex00/template.hpp
@@ -0,0 +1,8 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+template<typename T> void swap(T & a, T & b);
+template<typename T> T min(T & a, T & b);
+template<typename T> T max(T & a, T & b);
+
+#endif
